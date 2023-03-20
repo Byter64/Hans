250fe9292eacd6f9380d7797b8552c750149102d
Moreolo <coolrizzy100@gmail.com>
2023-03-20 16:42:39 +0100

    Programmzähler Testbench initial test

--- Prozessorentwurf/IceStudio/Prozessor/Testbenches/Programmzähler_tb.v
+++ Prozessorentwurf/IceStudio/Prozessor/Testbenches/Programmzähler_tb.v
@@ -34,15 +34,35 @@ module main_tb
   .ve305d5(ErhöhterPC_)
  );
  
+ always #1000 clock = ~clock;
+
  initial begin
   $dumpvars(0, main_tb);
  
   // TODO: initialize the registers here
   // e.g. value = 1;
   // e.g. #2 value = 0;
-  Schreibsignal = 0;
-  NeuerPC_ = 0;
- 
+  Schreibsignal = 0; NeuerPC_ = 0;
+  clock = 0;
+  
+  #900
+  Schreibsignal = 1; NeuerPC_ = 14;
+  #200
+  assert(AktuellerPC_, 14);
+  assert(ErhoehterPC_, 15);
+
+  #800
+  Schreibsignal = 0; NeuerPC_ = 4359;
+  #200
+  assert(AktuellerPC_, 15);
+  assert(ErhoehterPC_, 16);
+
+  #800
+  Schreibsignal = 1; NeuerPC_ = 67108862;
+  #200
+  assert(AktuellerPC_, 67108862);
+  assert(ErhoehterPC_, 67108863);
+
   #(DURATION) $display("End of simulation");
   $finish;
  end
