33c1eea4670688d815f20c5833a76d5b7e2f2648
Moreolo <coolrizzy100@gmail.com>
2023-06-05 20:33:17 +0200

    Steuerung erste Version
    Steuerung erstmal fertig geschrieben. Muss noch getestet werden.

--- Prozessorentwurf/Verilog/Prozessor/Steuerung.v
+++ Prozessorentwurf/Verilog/Prozessor/Steuerung.v
@@ -1,4 +1,9 @@
-module Steuerung (
+module Steuerung #(
+    parameter DECODETIME = 4,
+    parameter REGISTERWRITETIME = 2, //sollte die Register Schreib Zeit größer als die Alu Zeit sein, dann Blick in Dokumentation werfen
+    parameter ALUTIME = 4,
+    parameter PCWRITETIME = 1
+) (
     input [5:0] Funktionscode,
     input LoadBefehl,
     input StoreBefehl,
@@ -22,46 +27,132 @@ module Steuerung (
     output DekodierSignal
 );
 
-parameter DURATION = 10;
-parameter DECODETIME = 20;
-parameter REGISTERWRITETIME = 10;
-parameter ALUTIME = 20;
-parameter PCWRITETIME = 5;
+localparam FETCH = 2'b00;
+localparam DECODE = 2'b01;
+localparam ALU = 2'b10;
+localparam WRITEBACK = 2'b11;
 
-reg fetch;
-reg decode;
-reg alu;
-reg writeback;
+reg [1:0] state;
+reg [4:0] tick;
 
 always @(posedge Reset) begin
-    fetch <= 1;
-    decode <= 0;
-    alu <= 0;
-    writeback <= 0;
+    state <= FETCH;
+    tick <= 5'b00001;
 end
 
-always @(posedge fetch) begin
-    LoadBefehlSignal <= 1;
-end
+always @(posedge Clock) begin
+    case (state)
+        FETCH: begin
 
-always @(posedge BefehlGeladen) begin
-    if(fetch == 1) begin
-        LoadBefehlSignal <= 0;
-        fetch <= 0;
-        decode <= 1;
-    end
-end
+            if(LoadBefehlSignal == 0)
+                LoadBefehlSignal <= 1;
+            else if(BefehlGeladen == 1) begin
+                LoadBefehlSignal <= 0;
+                state <= DECODE;
+            end
 
-always @(posedge decode) begin
-    DekodierSignal <= 1;
-    #(DECODETIME)
-    DekodierSignal <= 0;
-    decode <= 0;
-    alu <= 1;
+        end
+        DECODE: begin
+
+            if(DekodierSignal == 0) begin 
+                DekodierSignal <= 1;
+                tick << DECODETIME;
+            end
+            else begin
+                tick >> 1;
+                if(tick[0] == 1) begin
+                    DekodierSignal <= 0;
+                    state <= ALU;
+                end
+            end
+
+        end
+        ALU: begin
+
+            if(ALUSignal == 0) begin
+                ALUSignal <= 1;
+                if(JALBefehl)
+                    RegisterSchreibSignal <= 1;
+                tick << ALUTIME;
+            end
+            else begin
+                tick >> 1;
+                if(tick[0] == 1) begin
+                    ALUSignal <= 0;
+                    if(JALBefehl)
+                        RegisterSchreibSignal <= 0;
+                    state <= WRITEBACK;
+                end
+            end
+        
+        end
+        WRITEBACK: begin
+
+            if(PCSignal == 0) begin
+                PCSignal <= 1;
+                if(UnbedingterSprungBefehl == 1 or BedingterSprungBefehl == 1)
+                    tick << PCWRITETIME;
+                else if(StoreBefehl == 1)
+                    StoreDatenSignal <= 1;
+                else if(LoadBefehl == 1)
+                    LoadDatenSignal <= 1;
+                else begin
+                    RegisterSchreibSignal <= 1;
+                    tick << REGISTERWRITETIME;
+                end
+            end
+            else begin
+                if(UnbedingterSprungBefehl == 1 or BedingterSprungBefehl == 1) begin
+                    tick >> 1;
+                    if(tick[0] == 1) begin
+                        PCSignal <= 0;
+                        state <= FETCH;
+                    end
+                end
+                else if(StoreBefehl == 1) begin
+                    if(DatenGespeichert == 1) begin
+                        PCSignal <= 0;
+                        StoreDatenSignal <= 0;
+                        state <= FETCH;
+                    end
+                end
+                else if(LoadBefehl == 1) begin
+                    if(DatenGeladen == 1) begin
+                        if(RegisterSchreibSignal == 0) begin
+                            RegisterSchreibSignal <= 1;
+                            tick << REGISTERWRITETIME;
+                        end
+                        else begin
+                            tick >> 1;
+                            if(tick[0] == 1) begin
+                                PCSignal <= 0;
+                                LoadDatenSignal <= 0;
+                                RegisterSchreibSignal <= 0;
+                                state <= FETCH;
+                            end
+                        end
+                    end
+                end
+                else begin
+                    tick >> 1;
+                    if(tick[0] == 1) begin
+                        PCSignal <= 0;
+                        RegisterSchreibSignal <= 0;
+                        state <= FETCH;
+                    end
+                end
+            end
+
+        end
+        default: begin
+            state <= FETCH;
+            tick <= 5'b00001;
+        end
+    endcase
 end
 
 always @(UnbedingterSprungBefehl, BedingterSprungBefehl, Bedingung) begin
-    if(UnbedingterSprungBefehl or (BedingterSprungBefehl and Bedingung))
+    if(UnbedingterSprungBefehl == 1 or (BedingterSprungBefehl == 1 and Bedingung == 1))
         PCSprungSignal <= 1;
     else
         PCSprungSignal <= 0;
