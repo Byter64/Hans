543a734c31e8f28792822e989c3b57964472c632
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2023-10-16 20:59:03 +0200

    Hinzugefügt: Kleines Programm in RAM als Startwert

--- Hardwareentwurf/Prozessor/TopModul.v
+++ Hardwareentwurf/Prozessor/TopModul.v
@@ -96,21 +96,6 @@ RAM #(
     .DatenGeschrieben(DatenGespeichert)
 );
 
-initial begin
-    InstruktionRAM.Daten[0] = 32'b10000000010000000000000000001111; //Addi R2, R0, 15
-    InstruktionRAM.Daten[1] = 32'b00000000001000000000000000000000; //Add R1, R0, R0
-    InstruktionRAM.Daten[2] = 32'b10000011111000000000000000000001; //Addi R31, R0, #1
-    InstruktionRAM.Daten[3] = 32'b10011011111111110000000000011111; //Sli R31, R31, #31
-
-    InstruktionRAM.Daten[4] = 32'b00000000001000010001000000000000; //Add R1, R1, R2
-    InstruktionRAM.Daten[5] = 32'b10000100010000100000000000000001; //Subi R2, R2, 1
-    InstruktionRAM.Daten[6] = 32'b00000000011000100000000000010000; //Ce R3, R2, R0
-    InstruktionRAM.Daten[7] = 32'b10110000001111110000000000000000; //Store R1, R31, #0
-    InstruktionRAM.Daten[8] = 32'b10111000000000111111111111111011; //Bez R3, -5
-
-    InstruktionRAM.Daten[9] = 32'b01000011111111111111111111111111; //jmp -1
-end
-
 integer resetTimer = 10;
 always @(posedge Clock) begin
     if(resetTimer > 0)begin
