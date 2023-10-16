2ebdf8bee343a2fc3d9848ab7fd81be66fdd740f
Moritz <coolrizzy100@gmail.com>
2023-10-16 19:49:33 +0200

    erste Logik fürs Lesen

--- Hardwareentwurf/Cache/Cache.v
+++ Hardwareentwurf/Cache/Cache.v
@@ -26,4 +26,27 @@ module Cache #(
 
 localparam BLOCKNUMBITS = CACHESIZEBITS - BLOCKSIZEBITS;
 localparam SETNUMBITS = BLOCKNUMBITS - NBITS;
+
+reg [31:0] data [2**CACHESIZEBITS-1 : 0];
+reg [31-BLOCKSIZEBITS-SETNUMBITS : 0] tags [2**BLOCKNUMBITS-1 : 0];
+
+wire [BLOCKSIZEBITS-1 : 0] block;
+wire [SETNUMBITS-1 : 0] set;
+wire [31-BLOCKSIZEBITS-SETNUMBITS : 0] tag;
+
+assign block = ProzessorAdresse[BLOCKSIZEBITS-1 : 0];
+assign set = ProzessorAdresse[BLOCKSIZEBITS+SETNUMBITS-1 : BLOCKSIZEBITS];
+assign tag = ProzessorAdresse[31 : BLOCKSIZEBITS+SETNUMBITS];
+
+always @* begin
+    if (ProzessorLesen) begin
+        index = 0
+        while (index < 2**(BLOCKNUMBITS - SETNUMBITS)) begin
+            if (tags[{index, set}] == tag)
+                ProzessorLesDaten = data[{index, set, block}];
+        end
+    end
+end
+
+
 endmodule
