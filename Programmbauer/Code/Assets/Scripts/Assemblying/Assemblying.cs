500fc7d32adebcc57fc2fb252fccfad826d703be
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2023-12-14 20:47:44 +0100

    Hinzugefügt: Linux version für vasm Geändert: Objektdateien werden nun in .../ObjectFiles abgelegt

--- Programmbauer/Code/Assets/Scripts/Assemblying/Assemblying.cs
+++ Programmbauer/Code/Assets/Scripts/Assemblying/Assemblying.cs
@@ -7,23 +7,52 @@ using UnityEngine;
 
 public class Assemblying : MonoBehaviour
 {
+    public static Assemblying Instance { get; private set; }
+    public const string nameOfOutputDirectory = "ObjectFiles";
+
     [SerializeField]
+    private string pathToAssemblerWindows;
+    [SerializeField]
+    private string pathToAssemblerLinux;
+
     private string pathToAssembler;
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        pathToAssembler = pathToAssemblerWindows;
+#elif UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
+        pathToAssembler = pathToAssemblerLinux;
+#endif
+    }
 
     public void AssembleInFolder(string path)
     {
         string[]allFilePaths = Directory.GetFiles(path);
         IEnumerable<string> assemblyFilePaths = allFilePaths.Where(x => x.EndsWith(".asm"));
+        if(assemblyFilePaths.Count() == 0 ) { return; }
+
+        string outputDirectory = assemblyFilePaths.FirstOrDefault();
+        outputDirectory = outputDirectory.Remove(outputDirectory.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+        outputDirectory += nameOfOutputDirectory + Path.DirectorySeparatorChar;
+        Directory.CreateDirectory(outputDirectory);
 
         foreach (string file in assemblyFilePaths)
         {
-            string outputFilePath = file.Remove(file.LastIndexOf('.')) + ".out";
-            string fullPathToAssembler = Application.dataPath + pathToAssembler;
+            string fileName = file.Substring(file.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+            string outputFilePath = outputDirectory + fileName.Remove(fileName.LastIndexOf('.')) + ".out";
+            string fullPathToAssembler = Application.dataPath + "/" + pathToAssembler;
             string arguments = file + " -Fhans -o " + outputFilePath;
             Log.Instance.Print(fullPathToAssembler + " " + arguments + "\n");
 
             Process assembler = new Process();
-            assembler.StartInfo.FileName = Application.dataPath + pathToAssembler;
+            assembler.StartInfo.FileName = Application.dataPath + "/"+ pathToAssembler;
             assembler.StartInfo.Arguments = arguments;
             assembler.StartInfo.UseShellExecute = false;
             assembler.StartInfo.RedirectStandardOutput = true;
