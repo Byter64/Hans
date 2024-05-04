f919dea7f307733a814aa6e501ee4aff42fb277c
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2024-05-04 19:23:28 +0200

    Pong angefangen

---
+++ Programme/Pong/GameInfo.asm
@@ -0,0 +1,8 @@
+.section GameInfo
+
+.set Spielbreite, 160
+.set Spielhoehe, 120
+.set SpielVersatzX, 10
+.set SpielVersatzY, 10
+.set SpielRandRechts, Spielbreite + SpielVersatzX
+.set SpielRandUnten, Spielhoehe + SpielVersatzY
---
+++ Programme/Pong/Start.asm
@@ -0,0 +1,7 @@
+.section Start
+
+Add R31, R0, R0 ;Stapelzeiger
+Subi R31, R31, -1
+
+Add R30, R0, R0 ;Bildpufferadresse
+Addis R30, R30, 0x8000
