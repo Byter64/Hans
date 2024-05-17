0d9309cfae4519c4eb1649a6dce478a3dc9cf062
Timo Grundheber <timo.grundheber@stud-mail.uni-wuerzburg.de>
2024-05-17 22:17:21 +0200

    Clock Hdmi +200000 Hz

--- Hardwareentwurf/TopModul.v
+++ Hardwareentwurf/TopModul.v
@@ -386,7 +386,7 @@ wire [3:0] clocks;
         .in_hz(25000000),
         .out0_hz(40000000),                 .out0_tol_hz(0),
         .out1_hz(50000000), .out1_deg( 90), .out1_tol_hz(0),
-        .out2_hz(25000000), .out2_deg(180), .out2_tol_hz(0),
+        .out2_hz(25200000), .out2_deg(180), .out2_tol_hz(0),
         .out3_hz(20000000), .out3_deg(300), .out3_tol_hz(0)
     )
     ecp5pll_inst
